5262879d3d98f21727907c94b5c62de4e39c7f4d
pranavmene2000 <pranavgenius2000@gmail.com>
2022-01-11 19:52:50 +0530

    cleanup: basic setup

--- pages/_app.js
+++ pages/_app.js
@@ -11,71 +11,57 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/nextjs-argon-dashboard.scss";
 
 Router.events.on("routeChangeStart", (url) => {
-  console.log(`Loading: ${url}`);
-  document.body.classList.add("body-page-transition");
-  ReactDOM.render(
-    <PageChange path={url} />,
-    document.getElementById("page-transition")
-  );
+	console.log(`Loading: ${url}`);
+	document.body.classList.add("body-page-transition");
+	ReactDOM.render(
+		<PageChange path={url} />,
+		document.getElementById("page-transition")
+	);
 });
 Router.events.on("routeChangeComplete", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
+	ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+	document.body.classList.remove("body-page-transition");
 });
 Router.events.on("routeChangeError", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
+	ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+	document.body.classList.remove("body-page-transition");
 });
 
 export default class MyApp extends App {
-  componentDidMount() {
-    let comment = document.createComment(`
+	componentDidMount() {
+		let comment = document.createComment(
+			`Copyrights Included ${new Date().getFullYear()}`
+		);
+		document.insertBefore(comment, document.documentElement);
+	}
+	static async getInitialProps({ Component, router, ctx }) {
+		let pageProps = {};
 
-=========================================================
-* * NextJS Argon Dashboard v1.1.0 based on Argon Dashboard React v1.1.0
-=========================================================
+		if (Component.getInitialProps) {
+			pageProps = await Component.getInitialProps(ctx);
+		}
 
-* Product Page: https://www.creative-tim.com/product/nextjs-argon-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/nextjs-argon-dashboard/blob/master/LICENSE.md)
+		return { pageProps };
+	}
 
-* Coded by Creative Tim
+	render() {
+		const { Component, pageProps } = this.props;
+		const Layout = Component.layout || (({ children }) => <>{children}</>);
 
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-`);
-    document.insertBefore(comment, document.documentElement);
-  }
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-  render() {
-    const { Component, pageProps } = this.props;
-
-    const Layout = Component.layout || (({ children }) => <>{children}</>);
-
-    return (
-      <React.Fragment>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>NextJS Argon Dashboard by Creative Tim</title>
-          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </React.Fragment>
-    );
-  }
+		return (
+			<React.Fragment>
+				<Head>
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1, shrink-to-fit=no"
+					/>
+					<title>NextJS Argon Dashboard by Creative Tim</title>
+					<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+				</Head>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</React.Fragment>
+		);
+	}
 }
