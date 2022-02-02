5a0d5200f8d1added2aac1dd8852a376be65bdea
Prathamesh Kurunkar <kurunkarprathamesh7@gmail.com>
2022-02-02 12:16:38 +0530

    fix: modify hashtag extraction using twitter-text

--- utils/formatter.js
+++ utils/formatter.js
@@ -1,25 +1,25 @@
+import twttr from 'twitter-text';
+
 export const removeLinkFromText = (string) => {
 	return string.replace(/(?:https?|ftp):\/\/[\n\S]+/g, "");
 };
 
 function getHashtags(searchText) {
-	const regexp = /(?:^|\W)#(\w+)(?!\w)/g;
-	const result = searchText.match(regexp);
-	if (result) return result;
-	return [];
+	return twttr.extractHashtags(searchText).map((tag)=>("#"+tag))
 }
 
-export const tweetFormatter = (tweet_full_text, tweet_obj) => {
+export const tweetFormatter = (tweet_full_text) => {
 	const hashtags = getHashtags(tweet_full_text);
 	let tweet = tweet_full_text;
-	hashtags.map(
-		(hashtag) =>
-			(tweet = tweet.replace(
-				hashtag,
-				`<span style="cursor: pointer;color: rgb(17, 109, 170);pointer-events: all" class="font-weight-bold">${hashtag}</span>`
-			))
-	);
-	return hashtags.length > 0
-		? removeLinkFromText(tweet)
-		: removeLinkFromText(tweet_full_text);
+	if(hashtags.length!==0){
+		hashtags.map(
+			(hashtag) =>
+				(tweet = tweet.replace(
+					hashtag,
+					`<span style="cursor: pointer;color: rgb(17, 109, 170);pointer-events: all" class="font-weight-bold">${hashtag}</span>`
+				))
+		);
+	}
+
+	return tweet;
 };
