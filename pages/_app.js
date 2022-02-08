0e8db7b803c90ed46a5e6c00ff5639279e3d5168
pranavmene2000 <pranavgenius2000@gmail.com>
2022-02-08 21:50:58 +0530

    feat: cool styling and emoji-mart component added to the publish page

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ import "assets/scss/nextjs-argon-dashboard.scss";
 import "@fullcalendar/common/main.min.css";
 import "@fullcalendar/daygrid/main.min.css";
 import "@fullcalendar/timegrid/main.min.css";
+import "emoji-mart/css/emoji-mart.css";
 
 import router from "next/router";
 
