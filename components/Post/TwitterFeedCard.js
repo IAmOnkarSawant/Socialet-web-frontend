ee8a770b1ca0101bacb80731fc179f21c5b34eb1
pranavmene2000 <pranavgenius2000@gmail.com>
2022-02-18 01:22:19 +0530

    fix: mapped images fixed

--- components/Post/TwitterFeedCard.js
+++ components/Post/TwitterFeedCard.js
@@ -122,163 +122,148 @@ function TwitterFeedCard({ tweet, search, feed, formik, ...props }, ref) {
 
   if (!ref) {
     return (
-      <section className="mb-4 rounded bg-white shadow-lg">
-        <div className="d-flex flex-row align-items-center justify-content-between px-3 py-3">
-          <div
-            className="d-flex flex-row align-items-center"
-            style={{ color: "rgb(54, 65, 65)" }}
-          >
-            <img
-              style={{ width: "35px", height: "35px" }}
-              className="mr-2 rounded-circle"
-              src={tweet.user.profile_image_url}
-            />
-            <div className="d-flex flex-column">
-              <div className="d-flex flex-row align-items-center">
-                <BsTwitter
-                  style={{ fontSize: "14px", color: "rgb(29, 161, 242)" }}
-                  className="mr-1"
-                />
-                <span
-                  style={{ fontSize: "13px" }}
-                  className="mr-1 font-weight-bolder"
-                >
-                  {tweet.user.name}
-                </span>
-                <span style={{ fontSize: "13px" }} className="mr-2">
-                  @{tweet.user.screen_name}
-                </span>
-                <span
-                  style={{
-                    fontSize: "12px",
-                    backgroundColor: "rgb(255, 198, 164)",
-                  }}
-                  className="mr-2 px-3 py-0 rounded-pill"
-                >
-                  {millify(tweet.user.followers_count)}
-                </span>
-              </div>
-              {search && (
-                <span style={{ fontSize: "12px" }}>
-                  {tweet.retweet_count === 0 ? "Tweet" : "Retweeted"}
-                </span>
-              )}
-              {feed && <span style={{ fontSize: "12px" }}>Tweet</span>}
-            </div>
-          </div>
-          <div className="pr-3">
-            <span style={{ fontSize: "13px" }}>
-              {moment(new Date(tweet.created_at)).startOf("day").fromNow()}
-            </span>
-          </div>
-        </div>
-        <hr className="mt-0 mb-1" />
-        <div style={{ paddingLeft: "53px" }} className="pr-3 py-3">
-          <span
-            style={{
-              fontSize: "15px",
-              color: "#364141",
-              whiteSpace: "pre-line",
-              wordBreak: "break-word",
-            }}
-            dangerouslySetInnerHTML={{
-              __html: tweetFormatter(tweet_text, tweet),
-            }}
-            onClick={clickHandler}
-          />
-        </div>
-        <div
-          style={{ paddingLeft: "53px" }}
-          className="d-flex flex-row align-items-center"
-        >
-          {tweet.retweet_count === 0
-            ? tweet?.entities?.media?.map(
-                (media) =>
-                  media.type == "photo" && (
-                    <div key={media.id} style={{ cursor: "pointer" }}>
-                      <img
-                        className="shadow-lg bg-white rounded-lg"
-                        width="140px"
-                        src={media.media_url_https}
-                        alt={media.display_url}
-                        onClick={() => setModalImageURL(media?.media_url_https)}
-                      />
-                    </div>
-                  )
-              )
-            : tweet?.retweeted_status?.entities.media?.map(
-                (media) =>
-                  media?.type == "photo" && (
-                    <div key={media.id} style={{ cursor: "pointer" }}>
-                      <img
-                        className="shadow-lg bg-white rounded-lg"
-                        width="140px"
-                        src={media.media_url_https}
-                        alt={media.display_url}
-                        onClick={() => setModalImageURL(media?.media_url_https)}
-                      />
-                    </div>
-                  )
-              )}
-        </div>
-        <div className="d-flex flex-row justify-content-end align-items-center p-3">
-          <div>
-            {tweetData.favorited ? (
-              <AiTwotoneHeart
-                style={{
-                  marginLeft: "auto",
-                  fontSize: 20,
-                  cursor: "pointer",
-                  color: "red",
-                }}
-                className="ml-4"
-                onClick={favTweetHandler}
-              />
-            ) : (
-              <AiOutlineHeart
-                style={{
-                  marginLeft: "auto",
-                  fontSize: 20,
-                  cursor: "pointer",
-                }}
-                className="ml-4"
-                onClick={favTweetHandler}
-              />
-            )}
-            <AiOutlineRetweet
-              style={{
-                marginLeft: "auto",
-                fontSize: 20,
-                cursor: "pointer",
-                color: tweetData.retweeted ? "#5e72e4" : "",
-              }}
-              className="ml-4"
-              onClick={reTweetHandler}
-            />
-            <BiPin
-              style={{
-                marginLeft: "auto",
-                fontSize: 20,
-                cursor: "pointer",
-              }}
-              className="ml-4"
-            />
-            <RiReplyLine
-              style={{
-                marginLeft: "auto",
-                fontSize: 20,
-                cursor: "pointer",
-              }}
-              className="ml-4"
-              onClick={replyTweetHandler}
-            />
-          </div>
-        </div>
-        {modalImageURL && (
-          <ModalImage setModalImageURL={setModalImageURL} url={modalImageURL} />
-        )}
-      </section>
-    );
+			<section className='mb-4 rounded bg-white shadow-lg'>
+				<div className='d-flex flex-row align-items-center justify-content-between px-3 py-3'>
+					<div
+						className='d-flex flex-row align-items-center'
+						style={{ color: "rgb(54, 65, 65)" }}
+					>
+						<img
+							style={{ width: "35px", height: "35px" }}
+							className='mr-2 rounded-circle'
+							src={tweet.user.profile_image_url}
+						/>
+						<div className='d-flex flex-column'>
+							<div className='d-flex flex-row align-items-center'>
+								<BsTwitter
+									style={{ fontSize: "14px", color: "rgb(29, 161, 242)" }}
+									className='mr-1'
+								/>
+								<span
+									style={{ fontSize: "13px" }}
+									className='mr-1 font-weight-bolder'
+								>
+									{tweet.user.name}
+								</span>
+								<span style={{ fontSize: "13px" }} className='mr-2'>
+									@{tweet.user.screen_name}
+								</span>
+								<span
+									style={{
+										fontSize: "12px",
+										backgroundColor: "rgb(255, 198, 164)",
+									}}
+									className='mr-2 px-3 py-0 rounded-pill'
+								>
+									{millify(tweet.user.followers_count)}
+								</span>
+							</div>
+							{search && (
+								<span style={{ fontSize: "12px" }}>
+									{tweet.retweet_count === 0 ? "Tweet" : "Retweeted"}
+								</span>
+							)}
+							{feed && <span style={{ fontSize: "12px" }}>Tweet</span>}
+						</div>
+					</div>
+					<div className='pr-3'>
+						<span style={{ fontSize: "13px" }}>
+							{moment(new Date(tweet.created_at)).startOf("day").fromNow()}
+						</span>
+					</div>
+				</div>
+				<hr className='mt-0 mb-1' />
+				<div style={{ paddingLeft: "53px" }} className='pr-3 py-3'>
+					<span
+						style={{
+							fontSize: "15px",
+							color: "#364141",
+							whiteSpace: "pre-line",
+							wordBreak: "break-word",
+						}}
+						dangerouslySetInnerHTML={{
+							__html: tweetFormatter(tweet_text, tweet),
+						}}
+						onClick={clickHandler}
+					/>
+				</div>
+				<div
+					style={{ paddingLeft: "53px" }}
+					className='d-flex flex-row align-items-center'
+				>
+					{tweet?.entities?.media?.map(
+						(media) =>
+							media.type == "photo" && (
+								<div key={media.id} style={{ cursor: "pointer" }}>
+									<img
+										className='shadow-lg bg-white rounded-lg'
+										width='140px'
+										src={media.media_url_https}
+										alt={media.display_url}
+										onClick={() => setModalImageURL(media?.media_url_https)}
+									/>
+								</div>
+							)
+					)}
+				</div>
+				<div className='d-flex flex-row justify-content-end align-items-center p-3'>
+					<div>
+						{tweetData.favorited ? (
+							<AiTwotoneHeart
+								style={{
+									marginLeft: "auto",
+									fontSize: 20,
+									cursor: "pointer",
+									color: "red",
+								}}
+								className='ml-4'
+								onClick={favTweetHandler}
+							/>
+						) : (
+							<AiOutlineHeart
+								style={{
+									marginLeft: "auto",
+									fontSize: 20,
+									cursor: "pointer",
+								}}
+								className='ml-4'
+								onClick={favTweetHandler}
+							/>
+						)}
+						<AiOutlineRetweet
+							style={{
+								marginLeft: "auto",
+								fontSize: 20,
+								cursor: "pointer",
+								color: tweetData.retweeted ? "#5e72e4" : "",
+							}}
+							className='ml-4'
+							onClick={reTweetHandler}
+						/>
+						<BiPin
+							style={{
+								marginLeft: "auto",
+								fontSize: 20,
+								cursor: "pointer",
+							}}
+							className='ml-4'
+						/>
+						<RiReplyLine
+							style={{
+								marginLeft: "auto",
+								fontSize: 20,
+								cursor: "pointer",
+							}}
+							className='ml-4'
+							onClick={replyTweetHandler}
+						/>
+					</div>
+				</div>
+				{modalImageURL && (
+					<ModalImage setModalImageURL={setModalImageURL} url={modalImageURL} />
+				)}
+			</section>
+		);
   }
 
   return (
