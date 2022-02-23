7e2ccd02dccc4a8c83570e949d8f4f76063a6b5e
pranavmene2000 <pranavgenius2000@gmail.com>
2022-02-23 21:23:25 +0530

    fix: boolean condition added to TwitterFeedCard for showing predicted emoji

--- components/Post/TwitterFeedCard.js
+++ components/Post/TwitterFeedCard.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { BsTwitter } from "react-icons/bs";
-import { BiPin } from "react-icons/bi";
 import { FaRetweet } from "react-icons/fa";
 import { RiReplyLine } from "react-icons/ri";
 import { AiOutlineHeart } from "react-icons/ai";
@@ -15,277 +14,281 @@ import { postFavorites, postReTweet } from "../../_api/twitter";
 import { emotions } from "../../utils/emotion";
 import { Spinner } from "reactstrap";
 
-function TwitterFeedCard({ tweet, search, feed, formik, ...props }, ref) {
-	const [modalImageURL, setModalImageURL] = useState("");
-	const { data: session } = useSession();
-	const [tweetData, setTweetData] = useState(tweet);
-	const tweet_text =
-		tweet?.retweet_count === 0
-			? tweet.full_text
-			: tweet.retweeted_status
-			? tweet.retweeted_status.full_text
-			: tweet.full_text;
+function TwitterFeedCard(
+  { tweet, search, feed, formik, isEmotionShow, ...props },
+  ref
+) {
+  const [modalImageURL, setModalImageURL] = useState("");
+  const { data: session } = useSession();
+  const [tweetData, setTweetData] = useState(tweet);
+  const tweet_text =
+    tweet?.retweet_count === 0
+      ? tweet.full_text
+      : tweet.retweeted_status
+      ? tweet.retweeted_status.full_text
+      : tweet.full_text;
 
-	const favTweetHandler = (e) => {
-		// already favorited-> to be unfavorited
-		if (tweetData.favorited) {
-			setTweetData({
-				...tweetData,
-				favorited: false,
-			});
-			const bodyData = {
-				tweet_id: tweet.id_str,
-				user_id: session.token.sub,
-				favorite: "False",
-			};
-			postFavorites(bodyData).then(({ data }) => {
-				console.log(data);
-			});
-			return;
-		}
-		// if not favorited
-		setTweetData({
-			...tweetData,
-			favorited: true,
-		});
-		const bodyData = {
-			tweet_id: tweet.id_str,
-			user_id: session.token.sub,
-			favorite: "True",
-		};
-		postFavorites(bodyData).then(({ data }) => {
-			console.log(data);
-		});
-	};
+  const favTweetHandler = (e) => {
+    // already favorited-> to be unfavorited
+    if (tweetData.favorited) {
+      setTweetData({
+        ...tweetData,
+        favorited: false,
+      });
+      const bodyData = {
+        tweet_id: tweet.id_str,
+        user_id: session.token.sub,
+        favorite: "False",
+      };
+      postFavorites(bodyData).then(({ data }) => {
+        console.log(data);
+      });
+      return;
+    }
+    // if not favorited
+    setTweetData({
+      ...tweetData,
+      favorited: true,
+    });
+    const bodyData = {
+      tweet_id: tweet.id_str,
+      user_id: session.token.sub,
+      favorite: "True",
+    };
+    postFavorites(bodyData).then(({ data }) => {
+      console.log(data);
+    });
+  };
 
-	const reTweetHandler = (e) => {
-		if (tweetData.retweeted) {
-			// un retweeting
-			setTweetData({
-				...tweetData,
-				retweeted: false,
-			});
-			const bodyData = {
-				tweet_id: tweet.id_str,
-				user_id: session.token.sub,
-				retweet: "False",
-			};
-			postReTweet(bodyData).then(({ data }) => {
-				console.log(data.message);
-			});
-			return;
-		}
-		// retweeting
-		setTweetData({
-			...tweetData,
-			retweeted: true,
-		});
-		const bodyData = {
-			tweet_id: tweet.id_str,
-			user_id: session.token.sub,
-			retweet: "True",
-		};
-		postReTweet(bodyData).then(({ data }) => {
-			console.log(data);
-		});
-	};
+  const reTweetHandler = (e) => {
+    if (tweetData.retweeted) {
+      // un retweeting
+      setTweetData({
+        ...tweetData,
+        retweeted: false,
+      });
+      const bodyData = {
+        tweet_id: tweet.id_str,
+        user_id: session.token.sub,
+        retweet: "False",
+      };
+      postReTweet(bodyData).then(({ data }) => {
+        console.log(data.message);
+      });
+      return;
+    }
+    // retweeting
+    setTweetData({
+      ...tweetData,
+      retweeted: true,
+    });
+    const bodyData = {
+      tweet_id: tweet.id_str,
+      user_id: session.token.sub,
+      retweet: "True",
+    };
+    postReTweet(bodyData).then(({ data }) => {
+      console.log(data);
+    });
+  };
 
-	const replyTweetHandler = (e) => {
-		router.push({
-			pathname: "/admin/publish",
-			query: {
-				replyTo: tweet.id_str,
-			},
-		});
-	};
+  const replyTweetHandler = (e) => {
+    router.push({
+      pathname: "/admin/publish",
+      query: {
+        replyTo: tweet.id_str,
+      },
+    });
+  };
 
-	const router = useRouter();
+  const router = useRouter();
 
-	const clickHandler = (e) => {
-		let el = e.target;
-		console.log(el.parentNode);
+  const clickHandler = (e) => {
+    let el = e.target;
+    console.log(el.parentNode);
 
-		if (el.parentNode.tagName !== "SPAN") {
-			e.preventDefault();
-			return;
-		}
+    if (el.parentNode.tagName !== "SPAN") {
+      e.preventDefault();
+      return;
+    }
 
-		// handling hashtag
-		if (
-			el &&
-			el.getAttribute("type") === "#hashtag" &&
-			el.innerText.trim().startsWith("#")
-		) {
-			router.push({
-				pathname: "/admin/search",
-				query: {
-					searchTerm: el.innerText.trim().replace("#", "hashtag"),
-				},
-			});
-		}
+    // handling hashtag
+    if (
+      el &&
+      el.getAttribute("type") === "#hashtag" &&
+      el.innerText.trim().startsWith("#")
+    ) {
+      router.push({
+        pathname: "/admin/search",
+        query: {
+          searchTerm: el.innerText.trim().replace("#", "hashtag"),
+        },
+      });
+    }
 
-		// handling userMention
-		if (
-			el &&
-			el.getAttribute("type") === "usermention" &&
-			el.innerText.trim().startsWith("@")
-		) {
-			router.push({
-				pathname: "/admin/userprofile",
-				query: {
-					username: el.innerText.trim().replace("@", ""),
-				},
-			});
-		}
-	};
+    // handling userMention
+    if (
+      el &&
+      el.getAttribute("type") === "usermention" &&
+      el.innerText.trim().startsWith("@")
+    ) {
+      router.push({
+        pathname: "/admin/userprofile",
+        query: {
+          username: el.innerText.trim().replace("@", ""),
+        },
+      });
+    }
+  };
 
-	return (
-		<section ref={ref} className='mb-4 rounded bg-white shadow-lg'>
-			<div className='d-flex flex-row align-items-center justify-content-between px-3 py-3'>
-				<div
-					className='d-flex flex-row align-items-center'
-					style={{ color: "rgb(54, 65, 65)" }}
-				>
-					<img
-						style={{ width: "35px", height: "35px" }}
-						className='mr-2 rounded-circle shadow-lg'
-						src={tweet.user.profile_image_url}
-					/>
-					<div className='d-flex flex-column'>
-						<div className='d-flex flex-row align-items-center'>
-							<BsTwitter
-								style={{ fontSize: "14px", color: "rgb(29, 161, 242)" }}
-								className='mr-1'
-							/>
-							<span
-								style={{ fontSize: "13px" }}
-								className='mr-1 font-weight-bolder'
-							>
-								{tweet.user.name}
-							</span>
-							<span style={{ fontSize: "13px" }} className='mr-2'>
-								@{tweet.user.screen_name}
-							</span>
-							{Number(tweet.user.followers_count) > 10 && (
-								<span
-									style={{
-										fontSize: "12px",
-										backgroundColor: "rgb(255, 198, 164)",
-									}}
-									className='mr-2 px-3 py-0 rounded-pill'
-								>
-									{millify(tweet.user.followers_count)}
-								</span>
-							)}
-						</div>
-						{search && (
-							<span style={{ fontSize: "12px" }}>
-								{tweet.retweet_count === 0 ? "Tweet" : "Retweeted"}
-							</span>
-						)}
-						{feed && <span style={{ fontSize: "12px" }}>Tweet</span>}
-					</div>
-				</div>
-				<div className='pr-3'>
-					<span style={{ fontSize: "13px" }}>
-						{moment(new Date(tweet.created_at)).startOf("second").fromNow()}
-					</span>
-				</div>
-			</div>
-			<hr className='mt-0 mb-1' />
-			<div style={{ paddingLeft: "53px" }} className='pr-3 py-3'>
-				<span
-					style={{
-						fontSize: "15px",
-						color: "#364141",
-						whiteSpace: "pre-line",
-						wordBreak: "break-word",
-					}}
-					dangerouslySetInnerHTML={{
-						__html: tweetFormatter(tweet_text, tweet),
-					}}
-					onClick={clickHandler}
-				/>
-			</div>
-			<div
-				style={{ paddingLeft: "53px" }}
-				className='d-flex flex-row align-items-center'
-			>
-				{tweet?.entities?.media?.map(
-					(media) =>
-						media.type == "photo" && (
-							<div key={media.id} style={{ cursor: "pointer" }}>
-								<img
-									className='shadow-lg bg-white rounded-lg mr-2'
-									width='140px'
-									src={media.media_url_https}
-									alt={media.display_url}
-									onClick={() => setModalImageURL(media?.media_url_https)}
-								/>
-							</div>
-						)
-				)}
-			</div>
-			<div className='d-flex flex-row justify-content-end align-items-center p-3'>
-				{tweet["emotion"] ? (
-					<span
-						style={{
-							marginLeft: "auto",
-							fontSize: 21,
-						}}
-					>
-						{emotions[tweet["emotion"]]}
-					</span>
-				) : (
-					!tweet["emotion"] && <Spinner type='grow' color='default' size='sm' />
-				)}
-				{tweetData.favorited ? (
-					<AiTwotoneHeart
-						style={{
-							marginLeft: "auto",
-							fontSize: 20,
-							cursor: "pointer",
-							color: "red",
-						}}
-						className='ml-4'
-						onClick={favTweetHandler}
-					/>
-				) : (
-					<AiOutlineHeart
-						style={{
-							marginLeft: "auto",
-							fontSize: 20,
-							cursor: "pointer",
-						}}
-						className='ml-4'
-						onClick={favTweetHandler}
-					/>
-				)}
-				<FaRetweet
-					style={{
-						marginLeft: "auto",
-						fontSize: 20,
-						cursor: "pointer",
-						color: tweetData.retweeted ? "#5e72e4" : "",
-					}}
-					className='ml-4'
-					onClick={reTweetHandler}
-				/>
-				<RiReplyLine
-					style={{
-						marginLeft: "auto",
-						fontSize: 20,
-						cursor: "pointer",
-					}}
-					className='ml-4'
-					onClick={replyTweetHandler}
-				/>
-			</div>
-			{modalImageURL && (
-				<ModalImage setModalImageURL={setModalImageURL} url={modalImageURL} />
-			)}
-		</section>
-	);
+  return (
+    <section ref={ref} className="mb-4 rounded bg-white shadow-lg">
+      <div className="d-flex flex-row align-items-center justify-content-between px-3 py-3">
+        <div
+          className="d-flex flex-row align-items-center"
+          style={{ color: "rgb(54, 65, 65)" }}
+        >
+          <img
+            style={{ width: "35px", height: "35px" }}
+            className="mr-2 rounded-circle shadow-lg"
+            src={tweet.user.profile_image_url}
+          />
+          <div className="d-flex flex-column">
+            <div className="d-flex flex-row align-items-center">
+              <BsTwitter
+                style={{ fontSize: "14px", color: "rgb(29, 161, 242)" }}
+                className="mr-1"
+              />
+              <span
+                style={{ fontSize: "13px" }}
+                className="mr-1 font-weight-bolder"
+              >
+                {tweet.user.name}
+              </span>
+              <span style={{ fontSize: "13px" }} className="mr-2">
+                @{tweet.user.screen_name}
+              </span>
+              {Number(tweet.user.followers_count) > 10 && (
+                <span
+                  style={{
+                    fontSize: "12px",
+                    backgroundColor: "rgb(255, 198, 164)",
+                  }}
+                  className="mr-2 px-3 py-0 rounded-pill"
+                >
+                  {millify(tweet.user.followers_count)}
+                </span>
+              )}
+            </div>
+            {search && (
+              <span style={{ fontSize: "12px" }}>
+                {tweet.retweet_count === 0 ? "Tweet" : "Retweeted"}
+              </span>
+            )}
+            {feed && <span style={{ fontSize: "12px" }}>Tweet</span>}
+          </div>
+        </div>
+        <div className="pr-3">
+          <span style={{ fontSize: "13px" }}>
+            {moment(new Date(tweet.created_at)).startOf("second").fromNow()}
+          </span>
+        </div>
+      </div>
+      <hr className="mt-0 mb-1" />
+      <div style={{ paddingLeft: "53px" }} className="pr-3 py-3">
+        <span
+          style={{
+            fontSize: "15px",
+            color: "#364141",
+            whiteSpace: "pre-line",
+            wordBreak: "break-word",
+          }}
+          dangerouslySetInnerHTML={{
+            __html: tweetFormatter(tweet_text, tweet),
+          }}
+          onClick={clickHandler}
+        />
+      </div>
+      <div
+        style={{ paddingLeft: "53px" }}
+        className="d-flex flex-row align-items-center"
+      >
+        {tweet?.entities?.media?.map(
+          (media) =>
+            media.type == "photo" && (
+              <div key={media.id} style={{ cursor: "pointer" }}>
+                <img
+                  className="shadow-lg bg-white rounded-lg mr-2"
+                  width="140px"
+                  src={media.media_url_https}
+                  alt={media.display_url}
+                  onClick={() => setModalImageURL(media?.media_url_https)}
+                />
+              </div>
+            )
+        )}
+      </div>
+      <div className="d-flex flex-row justify-content-end align-items-center p-3">
+        {isEmotionShow && tweet["emotion"] ? (
+          <span
+            style={{
+              marginLeft: "auto",
+              fontSize: 21,
+            }}
+          >
+            {emotions[tweet["emotion"]]}
+          </span>
+        ) : (
+          isEmotionShow &&
+          !tweet["emotion"] && <Spinner type="grow" color="default" size="sm" />
+        )}
+        {tweetData.favorited ? (
+          <AiTwotoneHeart
+            style={{
+              marginLeft: "auto",
+              fontSize: 20,
+              cursor: "pointer",
+              color: "red",
+            }}
+            className="ml-4"
+            onClick={favTweetHandler}
+          />
+        ) : (
+          <AiOutlineHeart
+            style={{
+              marginLeft: "auto",
+              fontSize: 20,
+              cursor: "pointer",
+            }}
+            className="ml-4"
+            onClick={favTweetHandler}
+          />
+        )}
+        <FaRetweet
+          style={{
+            marginLeft: "auto",
+            fontSize: 20,
+            cursor: "pointer",
+            color: tweetData.retweeted ? "#5e72e4" : "",
+          }}
+          className="ml-4"
+          onClick={reTweetHandler}
+        />
+        <RiReplyLine
+          style={{
+            marginLeft: "auto",
+            fontSize: 20,
+            cursor: "pointer",
+          }}
+          className="ml-4"
+          onClick={replyTweetHandler}
+        />
+      </div>
+      {modalImageURL && (
+        <ModalImage setModalImageURL={setModalImageURL} url={modalImageURL} />
+      )}
+    </section>
+  );
 }
 
 export default React.forwardRef(TwitterFeedCard);
--- pages/admin/feed.js
+++ pages/admin/feed.js
@@ -11,130 +11,138 @@ import { removeDuplicatesFromArrayOfObjects } from "../../utils/formatter";
 import { emotionRecogniser } from "../../_api/emotions";
 
 function Feed() {
-	const router = useRouter();
-	const { data: session } = useSession();
+  const router = useRouter();
+  const { data: session } = useSession();
 
-	const [page, setPage] = useState(1);
-	const { ref, inView } = useInView({
-		triggerOnce: true,
-		threshold: 1,
-	});
+  const [page, setPage] = useState(1);
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 1,
+  });
 
-	const [feed, setFeed] = useState([]);
-	const [hasMore, setHasMore] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
-	const [isNewPostAvailable, setIsNewPostAvailable] = useState(false);
+  const [feed, setFeed] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isNewPostAvailable, setIsNewPostAvailable] = useState(false);
 
-	const recogniseTweetEmotion = () => {
-		const tweets = feed.map((f) => ({
-			tweet: f.full_text,
-			id: f.id,
-		}));
-		emotionRecogniser({ tweets }).then(({ data }) => {
-			console.log(data.tweets);
-			const resp = feed.map((tweet) => {
-				const emoji = [...data.tweets].find((t) => {
-					if (t.id === tweet.id) {
-						return t;
-					}
-				});
-				console.log(emoji["emotion"]);
+  const recogniseTweetEmotion = () => {
+    const tweets = feed.map((f) => ({
+      tweet: f.full_text,
+      id: f.id,
+    }));
+    emotionRecogniser({ tweets }).then(({ data }) => {
+      console.log(data.tweets);
+      const resp = feed.map((tweet) => {
+        const emoji = [...data.tweets].find((t) => {
+          if (t.id === tweet.id) {
+            return t;
+          }
+        });
+        console.log(emoji["emotion"]);
 
-				return {
-					...tweet,
-					emotion: emoji["emotion"],
-				};
-			});
-			console.log(resp);
-			setFeed((previousFeed) => [...resp]);
-		});
-	};
+        return {
+          ...tweet,
+          emotion: emoji["emotion"],
+        };
+      });
+      console.log(resp);
+      setFeed((previousFeed) => [...resp]);
+    });
+  };
 
-	useEffect(() => {
-		if (isNewPostAvailable) {
-			recogniseTweetEmotion();
-			setIsNewPostAvailable(false);
-		}
-	}, [isNewPostAvailable]);
+  useEffect(() => {
+    if (isNewPostAvailable) {
+      recogniseTweetEmotion();
+      setIsNewPostAvailable(false);
+    }
+  }, [isNewPostAvailable]);
 
-	useEffect(() => {
-		if (inView && hasMore) setPage((page) => page + 1);
-	}, [inView]);
+  useEffect(() => {
+    if (inView && hasMore) setPage((page) => page + 1);
+  }, [inView]);
 
-	useEffect(() => {
-		const source = axios.CancelToken.source();
-		let isMounted = true;
-		setIsLoading(true);
-		getTwitterFeed(session.token.sub, page, { cancelToken: source.token })
-			.then(({ data }) => {
-				console.log(data);
-				if (isMounted) {
-					setFeed((prevFeed) => [
-						...removeDuplicatesFromArrayOfObjects(
-							[...prevFeed, ...data.feed],
-							"id"
-						),
-					]);
-					setHasMore(data.feed.length > 0);
-					setIsLoading(false);
-					setIsNewPostAvailable(data.feed.length > 0);
-				}
-			})
-			.catch((err) => {
-				if (!isMounted) return;
-				if (axios.isCancel(err)) console.log(err);
-				else console.log(err);
-			});
-		return () => {
-			isMounted = false;
-			source.cancel();
-		};
-	}, [page]);
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    let isMounted = true;
+    setIsLoading(true);
+    getTwitterFeed(session.token.sub, page, { cancelToken: source.token })
+      .then(({ data }) => {
+        console.log(data);
+        if (isMounted) {
+          setFeed((prevFeed) => [
+            ...removeDuplicatesFromArrayOfObjects(
+              [...prevFeed, ...data.feed],
+              "id"
+            ),
+          ]);
+          setHasMore(data.feed.length > 0);
+          setIsLoading(false);
+          setIsNewPostAvailable(data.feed.length > 0);
+        }
+      })
+      .catch((err) => {
+        if (!isMounted) return;
+        if (axios.isCancel(err)) console.log(err);
+        else console.log(err);
+      });
+    return () => {
+      isMounted = false;
+      source.cancel();
+    };
+  }, [page]);
 
-	return (
-		<React.Fragment>
-			<Navbar color='white' light expand='md'>
-				<NavbarBrand className='pl-4 font-weight-bold'>
-					Twitter feed
-				</NavbarBrand>
-				<Button
-					color='primary'
-					className='mb-3'
-					outline
-					style={{ marginLeft: "auto", marginRight: 15 }}
-					className='px-4'
-					size='sm'
-					onClick={() => router.back()}
-				>
-					Back
-				</Button>
-			</Navbar>
-			<Container className='mt-4' fluid='sm'>
-				{feed.length !== 0 &&
-					feed.map((tweet, index) => {
-						if (index === feed.length - 1) {
-							return (
-								<TwitterCard
-									ref={ref}
-									key={tweet.id}
-									tweet={tweet}
-									feed={true}
-								/>
-							);
-						}
-						return <TwitterCard key={tweet.id} tweet={tweet} feed={true} />;
-					})}
-				{isLoading && hasMore && (
-					<CenterSpinner
-						style={{ width: "100%", marginTop: 10, marginBottom: 20 }}
-						type='border'
-						size='md'
-						color='default'
-					/>
-				)}
-			</Container>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <Navbar color="white" light expand="md">
+        <NavbarBrand className="pl-4 font-weight-bold">
+          Twitter feed
+        </NavbarBrand>
+        <Button
+          color="primary"
+          className="mb-3"
+          outline
+          style={{ marginLeft: "auto", marginRight: 15 }}
+          className="px-4"
+          size="sm"
+          onClick={() => router.back()}
+        >
+          Back
+        </Button>
+      </Navbar>
+      <Container className="mt-4" fluid="sm">
+        {feed.length !== 0 &&
+          feed.map((tweet, index) => {
+            if (index === feed.length - 1) {
+              return (
+                <TwitterCard
+                  ref={ref}
+                  key={tweet.id}
+                  tweet={tweet}
+                  feed={true}
+                  isEmotionShow
+                />
+              );
+            }
+            return (
+              <TwitterCard
+                isEmotionShow
+                key={tweet.id}
+                tweet={tweet}
+                feed={true}
+              />
+            );
+          })}
+        {isLoading && hasMore && (
+          <CenterSpinner
+            style={{ width: "100%", marginTop: 10, marginBottom: 20 }}
+            type="border"
+            size="md"
+            color="default"
+          />
+        )}
+      </Container>
+    </React.Fragment>
+  );
 }
 
 Feed.requireAuth = true;
